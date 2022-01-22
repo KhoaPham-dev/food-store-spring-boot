ea27a8f0018c2d68df6367442261c7516504ed61
phongZo <lagiaphong.2001phong@gmail.com>
2022-01-22 23:45:17 +0700

    fix- ImportExport criteria - from to kind

--- source/com-landingis-api/src/main/java/com/landingis/api/storage/criteria/ImportExportCriteria.java
+++ source/com-landingis-api/src/main/java/com/landingis/api/storage/criteria/ImportExportCriteria.java
@@ -6,16 +6,17 @@ import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class ImportExportCriteria {
     private Long id;//
-    private String code;//
     private Integer categoryKind;//
     private Integer kind;//
-    private Long accountId;
     private Integer status;//
+    private Date from;
+    private Date to;
 
     public Specification<ImportExport> getSpecification() {
         return new Specification<ImportExport>() {
@@ -27,8 +28,11 @@ public class ImportExportCriteria {
                 if(getId() != null){
                     predicates.add(cb.equal(root.get("id"), getId()));
                 }
-                if (getCode() != null){
-                    predicates.add(cb.like(cb.lower(root.get("code")), "%" + getCode().toLowerCase() + "%"));
+                if(getFrom() != null){
+                    predicates.add(cb.greaterThanOrEqualTo(root.get("createdDate"), getFrom()));
+                }
+                if(getTo() != null){
+                    predicates.add(cb.lessThanOrEqualTo(root.get("createdDate"), getTo()));
                 }
                 if(getStatus() != null) {
                     predicates.add(cb.equal(root.get("status"), getStatus()));
@@ -39,10 +43,6 @@ public class ImportExportCriteria {
                 if(getCategoryKind() != null) {
                     Join<Category, ImportExport> joinCategory = root.join("category", JoinType.INNER);
                     predicates.add(cb.equal(cb.lower(joinCategory.get("kind")), getCategoryKind()));
-                }
-                if(getAccountId() != null) {
-                    Join<Account, ImportExport> joinAccount = root.join("account", JoinType.INNER);
-                    predicates.add(cb.equal(cb.lower(joinAccount.get("id")), getAccountId()));
                 }
                 return cb.and(predicates.toArray(new Predicate[predicates.size()]));
             }
